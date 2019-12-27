9c0c711d77dfdf4146d013bb2550c823e328f697
Tm-5 <Tm-5@192.168.1.38>
2019-12-27 20:09:04 +0530

    first

---
+++ src/main/java/com/tm/library/Sample.java
@@ -0,0 +1,36 @@
+package com.tm.library;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class Sample {
+/*	@Test
+	public void m1(){
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Tm-5\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://testingmasters.com/hrm/");
+		driver.findElement(By.id("txtUsername")).sendKeys("user02");
+		driver.findElement(By.id("txtPassword")).sendKeys("TM1234");
+        driver.findElement(By.id("btnLogin")).click();
+	
+	}*/
+
+public static void main(String[] args) {
+	
+
+	String s="kishore";
+	int i=0;
+	for(char c:s.toCharArray()){
+		
+		i++;
+		
+	}
+	System.out.print(i);
+}		
+}
+
+
+	
+	
+
